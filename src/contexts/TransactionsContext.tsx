040a3ea6e0e28e6be09a3f4370b8ff1b0e884e8f
Joao Vaz <joaomarcelovaz@lancenet.com.br>
2023-05-25 21:04:11 -0300

    Projeto 3

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,82 @@
+import { ReactNode, useEffect, useState, useCallback } from 'react'
+import { createContext } from 'use-context-selector'
+import { api } from '../lib/axios'
+
+interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: 'strring'
+  createdAt: 'string'
+}
+
+interface CreateTransactionInput {
+  description: string
+  price: number
+  category: string
+  type: 'income' | 'outcome'
+}
+
+interface TransactionContextType {
+  transactions: Transaction[]
+  fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (data: CreateTransactionInput) => Promise<void>
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType)
+
+interface TransactionsProviderProps {
+  children: ReactNode
+}
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  // dois modos de fazer o fetch, um com async e outro sem
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/transactions')
+  //     .then(response => response.json())
+  //     .then(data =>{
+  //       console.log(data)
+  //     })
+  // }, [])
+
+  const fetchTransactions = useCallback(async (query?: string) => {
+    const response = await api.get('transactions', {
+      params: {
+        _sort: 'createdAt',
+        _order: 'desc',
+        q: query,
+      },
+    })
+    setTransactions(response.data)
+  }, [])
+
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, category, price, type } = data
+      const response = await api.post('transactions', {
+        description,
+        category,
+        price,
+        type,
+        createdAt: new Date(),
+      })
+      setTransactions((state) => [response.data, ...state])
+    },
+    [],
+  )
+
+  useEffect(() => {
+    fetchTransactions()
+  }, [fetchTransactions])
+
+  return (
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransactions, createTransaction }}
+    >
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,52 @@
+import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { SearchFormContainer } from './styles'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContextSelector } from 'use-context-selector'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { memo } from 'react'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+function SearchFormComponent() {
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
+
+export const SearchForm = memo(SearchFormComponent)
